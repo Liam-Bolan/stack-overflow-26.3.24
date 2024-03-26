22224c0fbd0ea441ad37b7c5309b205d1e4ad9b6
Liam Bolan <AQ231688@aquinas.ac.uk>
2024-03-26 13:45:05 +0000

    Add project files.

---
+++ stack overflow 26.3.24/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack_overflow_26._3._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number (e.g. '5'): ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nFact: {FactorialCalc(n)}");
+            Console.WriteLine($"\nFib: {Fibonacci(n)}");
+            
+            Console.Write("\nEnter any string: ");
+            string s = Console.ReadLine();
+            Console.Write($"\nPalindrome? {PalCheck(s)}");
+            Console.ReadLine();
+        }
+        static int FactorialCalc(int n)
+        {
+           if(n == 1)
+           {
+                return 1;
+           }
+           else
+           {
+                return n * FactorialCalc(n - 1);
+           }
+
+        }
+        static int Fibonacci(int n)
+        {
+            if(n < 3)
+            {
+                return 1;
+            }
+            else 
+            {
+                return n = Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+        static bool PalCheck(string s)
+        {
+            if(s.Length < 2)
+            {
+                return true;
+            }
+            if (s[0] != s[s.Length - 1])
+            {
+                return false;
+                
+            }
+            return PalCheck(s.Substring(1, s.Length - 2));
+
+        } 
+    }
+}
